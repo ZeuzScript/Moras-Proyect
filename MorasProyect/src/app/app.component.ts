f4a45b7580e927098c24194bf2994ed029968561
ZeuzScript <cermija@gmail.com>
2025-01-31 13:02:42 -0600

    Se subio el avance del proyecto que tenia en local

---
+++ MorasProyect/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { WelcomeComponent } from '../components/welcome/welcome.component';
+import { SocialMediaComponent } from '../components/social-media/social-media.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ToolbarComponent,
+    WelcomeComponent,
+    SocialMediaComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'MorasProyect';
+}
---
+++ MorasProyect/src/components/social-media/social-media.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { SocialMediaElementComponent } from './social-media-element/social-media-element.component';
+import { SocialMediaElement } from './models';
+import { MainContainerComponent } from '../layout';
+
+@Component({
+  selector: 'app-social-media',
+  standalone: true,
+  imports: [SocialMediaElementComponent, MainContainerComponent],
+  templateUrl: './social-media.component.html',
+  styleUrl: './social-media.component.scss'
+})
+export class SocialMediaComponent {
+  socialMediaList: SocialMediaElement[] = [
+    {
+      name: 'Twitch',
+      link: 'https://www.twitch.tv/gentelman_programming',
+      imgPath: 'assets/images/social-media/twitch.svg',
+    },
+    {
+      name: 'Youtube',
+      link: 'https://www.youtube.com/',
+      imgPath: 'assets/images/social-media/youtube.svg',
+    },
+    {
+      name: 'Discord',
+      link: 'https://discord.gg/',
+      imgPath: 'assets/images/social-media/discord.svg',
+    },
+    {
+      name:'X',
+      link: 'https://twitter.com/',
+      imgPath: 'assets/images/social-media/twitter.svg',
+    },
+    {
+      name: 'Instagram',
+      link: 'https://www.instagram.com/',
+      imgPath: 'assets/images/social-media/instagam.svg',
+    },
+    {
+      name: 'TikTok',
+      link: 'https://www.tiktok.com/',
+      imgPath: 'assets/images/social-media/tiktok.svg',
+    },
+    {
+      name: 'OnlyFans',
+      link: 'https://onlyfans.com/',
+      imgPath: 'assets/images/social-media/onlyfans.svg'
+    }
+  ];
+
+}
